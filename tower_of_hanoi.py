ae9ce9e53b36eee5bb561f6bfecdb006ff72680a
Farrabi <55173236+Farrabi@users.noreply.github.com>
2020-03-27 11:24:13 +0900

    Update tower_of_hanoi.py

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -4,14 +4,18 @@
 #instructions to a robot to move disc from one tower to another. 
 #m representing the number of discs in the intial tower (tower A)
 
-def hanoi_solver(m,y,f,a):
+def move(a,f):
+	print("Move the disc from {} to {}".format(a, f))
+
+
+def hanoi_solver(m,a,y,f):
 	if m==0:
 		pass
 	else:
-		hanoi_solver(m-1,y,a,f)
+		hanoi_solver(m-1,a,f,y)
 		move(a,f)
-		hanoi_solver(m-1,f,y,a)
+		hanoi_solver(m-1,y,a,f)
 
-hanoi_solver(5, "A", "B", "C")
+hanoi_solver(6, "A", "B", "C")
 
 
